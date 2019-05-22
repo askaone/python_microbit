8542c50be54d4b469763982ced4df23ce623b8dc
Juan Pablo Parra <jsandova2@gmail.com>
2019-05-23 04:30:11 +0900

    around the world now works for left and right, and the code is more clean... buuut, ignore the initial coodinate u.u

--- microbit_challenges.py
+++ microbit_challenges.py
@@ -279,39 +279,38 @@ def around_the_world(direction, start_position):
     x = start_position[0]
     y = start_position[1]
 
+    directions = None
+    coordinates = None
+    if direction == RIGHT:
+        coordinates = {UP:(0, 4), RIGHT:(0, 0), DOWN:(4, 0), LEFT:(4, 4)}
+        if x == 0 and 0 <= y <= 4:
+            directions = [UP, RIGHT, DOWN, LEFT]
+        elif x == 4 and 0 <= y <= 4:
+            directions = [DOWN, LEFT, UP, RIGHT]
+        elif y == 0 and 0 <= x <= 4:
+            directions = [RIGHT, DOWN, LEFT, UP]
+        elif y == 4 and 0 <= x <= 4:
+            directions = [LEFT, UP, RIGHT, DOWN]
+    else:
+        coordinates = {UP:(4, 4), RIGHT:(0, 4), DOWN:(0, 0), LEFT:(4, 0)}
+        if x == 0 and 0 <= y <= 4:
+            directions = [DOWN, RIGHT, UP, LEFT]
+        elif x == 4 and 0 <= y <= 4:
+            directions = [UP, LEFT, DOWN, RIGHT]
+        elif y == 0 and 0 <= x <= 4:
+            directions = [LEFT, DOWN, RIGHT, UP]
+        elif y == 4 and 0 <= x <= 4:
+            directions = [RIGHT, UP, LEFT, DOWN]
+
+    counter = 0
     while True:
-        if x < 5 and y == 0: # Top
-            if direction == RIGHT:
-                clear_turn_on_horizontal(RIGHT, x, y)
-                x = 4
-            elif direction == LEFT:
-                clear_turn_on_horizontal(LEFT, x, y)
-                x = 0
-
-        if x == 4 and y < 5: # Right
-            if direction == RIGHT:
-                clear_turn_on_vertical(DOWN, x, y)
-                y = 4
-            if direction == LEFT:
-                clear_turn_on_vertical(UP, x, y)
-                y = 0
-
-        if x >= 0 and y == 4: # Bottom
-            if direction == RIGHT:
-                clear_turn_on_horizontal(LEFT, x, y)
-                x = 0
-            if direction == LEFT:
-                clear_turn_on_horizontal(RIGHT, x, y)
-                x = 4
-
-        if x == 0 and y >= 0: # Left
-            if direction == RIGHT:
-                clear_turn_on_vertical(UP, x, y)
-                y = 0
-            if direction == LEFT:
-                clear_turn_on_vertical(DOWN, x, y)
-                y = 0
+        for sDirection in directions:
+            coor = coordinates[sDirection]
+            if sDirection == RIGHT or sDirection == LEFT:
+                clear_turn_on_horizontal(sDirection, coor[0], coor[1])
+            elif sDirection == UP or sDirection == DOWN:
+                clear_turn_on_vertical(sDirection, coor[0], coor[1])
 
 
-around_the_world(LEFT, (3, 0))
+around_the_world(LEFT, (0, 0))
 """
